ff5f4f24d2a7b86f06a651419272d058eb97ca0c
sarahsakinah <91169310+sarahsakinah@users.noreply.github.com>
2021-12-10 18:05:32 +0700

    Add files via upload

---
+++ sourcecode praktikum 9 elkom 1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 17:56:30 2021
+
+nama :sarah sakinah
+nim :065002100033
+prodi:sistem informasi
+"""
+print('=========== ELKOM 1 ===========')
+list=[2,4,3,2,11,21]
+print(list)
+print('hasil reverse ke tuple')
+print(tuple(list))
